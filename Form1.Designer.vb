de2273e4fa42d2e15194f3dc54500d6b5c4911db
dilerb <babil_ovuncdiler@hotmail.com>
2018-08-13 17:12:59 +0300

    quota hunter
    Grab the free quata for a giving course in ITU course select system.

---
+++ Form1.Designer.vb
@@ -0,0 +1,352 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label15 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(279, 256)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(80, 47)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "BAŞLAT"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(198, 54)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "BLG"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 5000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(134, 135)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Kontenjan:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(198, 135)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "?"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(198, 94)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox2.TabIndex = 7
+        Me.TextBox2.Text = "11788"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(159, 97)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "CRN:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(13, 15)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(893, 472)
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(128, 57)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Bölüm Kodu"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(751, 437)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(132, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "made by Babil Övünç Diler"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(185, 270)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox3.TabIndex = 13
+        Me.TextBox3.Text = "5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(38, 273)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(141, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Kaç saniyede bir sorgulasın?"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(360, 57)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(64, 13)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Bölüm Kodu"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(391, 97)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(33, 13)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "CRN:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(430, 94)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox4.TabIndex = 18
+        Me.TextBox4.Text = "11788"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(430, 135)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(13, 13)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "?"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(366, 135)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(58, 13)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "Kontenjan:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(430, 54)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox5.TabIndex = 15
+        Me.TextBox5.Text = "BLG"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(592, 57)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(64, 13)
+        Me.Label11.TabIndex = 26
+        Me.Label11.Text = "Bölüm Kodu"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(623, 97)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(33, 13)
+        Me.Label12.TabIndex = 25
+        Me.Label12.Text = "CRN:"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(662, 94)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox6.TabIndex = 24
+        Me.TextBox6.Text = "11788"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(662, 135)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(13, 13)
+        Me.Label13.TabIndex = 23
+        Me.Label13.Text = "?"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(598, 135)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(58, 13)
+        Me.Label14.TabIndex = 22
+        Me.Label14.Text = "Kontenjan:"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(662, 54)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox7.TabIndex = 21
+        Me.TextBox7.Text = "BLG"
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 5000
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 5000
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 1000
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(775, 460)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(108, 13)
+        Me.Label15.TabIndex = 27
+        Me.Label15.Text = "thanks to Cihan Çırak"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange
+        Me.ClientSize = New System.Drawing.Size(920, 496)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Kontenjan Avcısı v0.58"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+    Friend WithEvents Timer4 As System.Windows.Forms.Timer
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,257 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Enabled = False
+        Timer2.Enabled = False
+        Timer3.Enabled = False
+        Timer4.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        If Timer1.Enabled = "True" Then
+            Timer1.Interval = (Int(TextBox3.Text) * 1000)
+            Timer1.Enabled = False
+            Timer2.Interval = (Int(TextBox3.Text) * 1000)
+            Timer2.Enabled = False
+            Timer3.Interval = (Int(TextBox3.Text) * 1000)
+            Timer3.Enabled = False
+            Timer4.Enabled = False
+            Button1.Text = "BAŞLAT"
+        Else
+            Timer1.Enabled = True
+            Timer2.Enabled = True
+            Timer3.Enabled = True
+            Button1.Text = "DURDUR"
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        If TextBox1.Text = "" Then
+            Label2.Text = "?"
+            GoTo e1
+        End If
+
+        Dim Talep As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create("http://www.sis.itu.edu.tr/tr/ders_programlari/LSprogramlar/prg.php?fb=" & TextBox1.Text)
+        Dim Cevap As System.Net.HttpWebResponse = Talep.GetResponse()
+
+
+        Dim Okuyucu As System.IO.StreamReader = New System.IO.StreamReader(Cevap.GetResponseStream())
+
+
+        Dim Kaynakkodu As String = Okuyucu.ReadToEnd
+
+
+
+        Dim sayi1
+        Dim w1, w2
+        Dim kont As Integer
+
+
+        If TextBox2.Text = "" Then
+            Label2.Text = "?"
+            GoTo e1
+        End If
+
+        sayi1 = InStr(1, Kaynakkodu, TextBox2.Text)
+
+
+
+        Kaynakkodu = Strings.Mid(Kaynakkodu, sayi1, 700)
+        sayi1 = InStr(1, Kaynakkodu, "<br>")
+        Kaynakkodu = Strings.Mid(Kaynakkodu, sayi1, (700 - sayi1))
+        sayi1 = InStr(1, Kaynakkodu, "<a")
+        Kaynakkodu = Strings.Mid(Kaynakkodu, 1, sayi1)
+        sayi1 = InStr(1, Kaynakkodu, "<td>")
+        Kaynakkodu = Strings.Mid(Kaynakkodu, (sayi1 + 4), (Len(Kaynakkodu) - sayi1))
+        sayi1 = InStr(1, Kaynakkodu, "<td>")
+        Kaynakkodu = Strings.Mid(Kaynakkodu, (sayi1 + 4), (Len(Kaynakkodu) - sayi1))
+        sayi1 = InStr(1, Kaynakkodu, "<td>")
+        Kaynakkodu = Strings.Mid(Kaynakkodu, (sayi1 + 4), (Len(Kaynakkodu) - sayi1))
+        sayi1 = InStr(1, Kaynakkodu, "<td>")
+        Kaynakkodu = Strings.Mid(Kaynakkodu, (sayi1 + 4), (Len(Kaynakkodu) - sayi1))
+        sayi1 = InStr(1, Kaynakkodu, "<")
+        w1 = Strings.Left(Kaynakkodu, sayi1 - 1)
+        sayi1 = InStr(1, Kaynakkodu, "<td>")
+        Kaynakkodu = Strings.Mid(Kaynakkodu, (sayi1 + 4), (Len(Kaynakkodu) - sayi1))
+        sayi1 = InStr(1, Kaynakkodu, "<")
+        w2 = Strings.Left(Kaynakkodu, sayi1 - 1)
+
+        kont = Int(w1) - Int(w2)
+
+
+
+        If kont > 0 Then
+            Label2.Text = kont & " kişilik yer var " & "(" & w1 & "/" & w2 & ")"
+            Timer4.Enabled = True
+
+        End If
+
+        If kont = 0 Then
+            Label2.Text = "Dolu " & "(" & w1 & "/" & w2 & ")"
+
+        End If
+
+
+        If kont < 0 Then
+            Label2.Text = w1 & "/" & w2 & " ..."
+
+        End If
+
+e1:
+    End Sub
+
+    
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+
+        If TextBox5.Text = "" Then
+            Label9.Text = "?"
+            GoTo e2
+
+        End If
+
+
+        Dim Talep2 As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create("http://www.sis.itu.edu.tr/tr/ders_programlari/LSprogramlar/prg.php?fb=" & TextBox5.Text)
+        Dim Cevap2 As System.Net.HttpWebResponse = Talep2.GetResponse()
+       
+
+        Dim Okuyucu2 As System.IO.StreamReader = New System.IO.StreamReader(Cevap2.GetResponseStream())
+
+
+        Dim Kaynakkodu2 As String = Okuyucu2.ReadToEnd
+
+        Dim sayi2
+        Dim w12, w22
+        Dim kont2 As Integer
+
+        If TextBox4.Text = "" Then
+            Label9.Text = "?"
+            GoTo e2
+        End If
+
+        sayi2 = InStr(1, Kaynakkodu2, TextBox4.Text)
+
+        Kaynakkodu2 = Strings.Mid(Kaynakkodu2, sayi2, 700)
+        sayi2 = InStr(1, Kaynakkodu2, "<br>")
+        Kaynakkodu2 = Strings.Mid(Kaynakkodu2, sayi2, (700 - sayi2))
+        sayi2 = InStr(1, Kaynakkodu2, "<a")
+        Kaynakkodu2 = Strings.Mid(Kaynakkodu2, 1, sayi2)
+        sayi2 = InStr(1, Kaynakkodu2, "<td>")
+        Kaynakkodu2 = Strings.Mid(Kaynakkodu2, (sayi2 + 4), (Len(Kaynakkodu2) - sayi2))
+        sayi2 = InStr(1, Kaynakkodu2, "<td>")
+        Kaynakkodu2 = Strings.Mid(Kaynakkodu2, (sayi2 + 4), (Len(Kaynakkodu2) - sayi2))
+        sayi2 = InStr(1, Kaynakkodu2, "<td>")
+        Kaynakkodu2 = Strings.Mid(Kaynakkodu2, (sayi2 + 4), (Len(Kaynakkodu2) - sayi2))
+        sayi2 = InStr(1, Kaynakkodu2, "<td>")
+        Kaynakkodu2 = Strings.Mid(Kaynakkodu2, (sayi2 + 4), (Len(Kaynakkodu2) - sayi2))
+        sayi2 = InStr(1, Kaynakkodu2, "<")
+        w12 = Strings.Left(Kaynakkodu2, sayi2 - 1)
+        sayi2 = InStr(1, Kaynakkodu2, "<td>")
+        Kaynakkodu2 = Strings.Mid(Kaynakkodu2, (sayi2 + 4), (Len(Kaynakkodu2) - sayi2))
+        sayi2 = InStr(1, Kaynakkodu2, "<")
+        w22 = Strings.Left(Kaynakkodu2, sayi2 - 1)
+
+        kont2 = Int(w12) - Int(w22)
+
+
+        If kont2 > 0 Then
+            Label9.Text = kont2 & " kişilik yer var " & "(" & w12 & "/" & w22 & ")"
+            Timer4.Enabled = True
+
+        End If
+
+
+
+        If kont2 = 0 Then
+            Label9.Text = "Dolu " & "(" & w12 & "/" & w22 & ")"
+
+        End If
+
+
+
+
+        If kont2 < 0 Then
+            Label9.Text = w12 & "/" & w22 & " ..."
+
+        End If
+
+
+e2:
+    End Sub
+
+    Private Sub Timer3_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer3.Tick
+
+        If TextBox7.Text = "" Then
+            Label13.Text = "?"
+            GoTo e3
+        End If
+
+        Dim Talep3 As System.Net.HttpWebRequest = System.Net.HttpWebRequest.Create("http://www.sis.itu.edu.tr/tr/ders_programlari/LSprogramlar/prg.php?fb=" & TextBox7.Text)
+        Dim Cevap3 As System.Net.HttpWebResponse = Talep3.GetResponse()
+       
+        Dim Okuyucu3 As System.IO.StreamReader = New System.IO.StreamReader(Cevap3.GetResponseStream())
+        
+        Dim Kaynakkodu3 As String = Okuyucu3.ReadToEnd
+        Dim sayi3
+        Dim w13, w23
+        Dim kont3 As Integer
+
+        If TextBox6.Text = "" Then
+            Label13.Text = "?"
+            GoTo e3
+        End If
+
+        
+        sayi3 = InStr(1, Kaynakkodu3, TextBox6.Text)
+
+        Kaynakkodu3 = Strings.Mid(Kaynakkodu3, sayi3, 700)
+        sayi3 = InStr(1, Kaynakkodu3, "<br>")
+        Kaynakkodu3 = Strings.Mid(Kaynakkodu3, sayi3, (700 - sayi3))
+        sayi3 = InStr(1, Kaynakkodu3, "<a")
+        Kaynakkodu3 = Strings.Mid(Kaynakkodu3, 1, sayi3)
+        sayi3 = InStr(1, Kaynakkodu3, "<td>")
+        Kaynakkodu3 = Strings.Mid(Kaynakkodu3, (sayi3 + 4), (Len(Kaynakkodu3) - sayi3))
+        sayi3 = InStr(1, Kaynakkodu3, "<td>")
+        Kaynakkodu3 = Strings.Mid(Kaynakkodu3, (sayi3 + 4), (Len(Kaynakkodu3) - sayi3))
+        sayi3 = InStr(1, Kaynakkodu3, "<td>")
+        Kaynakkodu3 = Strings.Mid(Kaynakkodu3, (sayi3 + 4), (Len(Kaynakkodu3) - sayi3))
+        sayi3 = InStr(1, Kaynakkodu3, "<td>")
+        Kaynakkodu3 = Strings.Mid(Kaynakkodu3, (sayi3 + 4), (Len(Kaynakkodu3) - sayi3))
+        sayi3 = InStr(1, Kaynakkodu3, "<")
+        w13 = Strings.Left(Kaynakkodu3, sayi3 - 1)
+        sayi3 = InStr(1, Kaynakkodu3, "<td>")
+        Kaynakkodu3 = Strings.Mid(Kaynakkodu3, (sayi3 + 4), (Len(Kaynakkodu3) - sayi3))
+        sayi3 = InStr(1, Kaynakkodu3, "<")
+        w23 = Strings.Left(Kaynakkodu3, sayi3 - 1)
+
+        kont3 = Int(w13) - Int(w23)
+
+        
+        If kont3 > 0 Then
+            Label13.Text = kont3 & " kişilik yer var " & "(" & w13 & "/" & w23 & ")"
+            Timer4.Enabled = True
+        End If
+            
+
+        
+        If kont3 = 0 Then
+
+            Label13.Text = "Dolu " & "(" & w13 & "/" & w23 & ")"
+        End If
+
+
+        
+        If kont3 < 0 Then
+            Label13.Text = w13 & "/" & w23 & " ..."
+
+        End If
+
+
+e3:
+    End Sub
+
+    Private Sub Timer4_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer4.Tick
+        My.Computer.Audio.PlaySystemSound(System.Media.SystemSounds.Hand)
+    End Sub
+End Class
